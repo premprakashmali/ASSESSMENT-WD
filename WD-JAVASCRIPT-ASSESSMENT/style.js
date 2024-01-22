416956f4e2c500a0dde7e11caf1b25394f237eed
premprakashmali <144773206+premprakashmali@users.noreply.github.com>
2024-01-22 15:49:45 +0530

    Add files via upload
    WD JavaScript Essentials And React -Advanced

---
+++ WD-JAVASCRIPT-ASSESSMENT/style.js
@@ -0,0 +1,25 @@
+function myfunction() {
+    var bookingtype = document.getElementById("bookingtype").value;
+    var slot = document.getElementById("slot-1");
+    var time = document.getElementById("time-1");
+    var date = document.getElementById("date-1");
+
+    if (bookingtype === "fullDay") {
+        slot.style.display = "none";
+        time.style.display = "none";
+        date.style.display = "block";
+    } else if (bookingtype === "halfDay") {
+        slot.style.display = "block";
+        date.style.display = "block";
+        time.style.display = "none";
+
+    } else if (bookingtype === "hourly") {
+        slot.style.display = "none";
+        time.style.display = "block";
+        date.style.display = "block";
+    }
+    else{
+
+       window.alert("form submited")
+    }
+}
